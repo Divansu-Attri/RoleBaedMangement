659c8ec385192a60936dbe601d7c9ba1f8de2e08
Divansu <divansu@xcrino.com>
2025-05-16 16:59:00 +0530

    final updates

--- server/server.js
+++ server/server.js
@@ -15,14 +15,26 @@ const PORT = process.env.PORT || 5000;
 // Connect to MongoDB
 connectDB();
 
-const corsOptions = {
-  origin:["http://localhost:5173", "https://role-baed-mangement-project.vercel.app/", "https://role-baed-mangement-65pduhnhz-divansus-projects.vercel.app/"],
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://role-baed-mangement-project.vercel.app",
+  "https://role-baed-mangement-65pduhnhz-divansus-projects.vercel.app"
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods:"GET,PUT,PATCH,DELETE,POST,HEAD",
   credentials:true
-}
+}));
 
-// Middleware
-app.use(cors(corsOptions));
 app.use(express.json());
 
 // Routes
